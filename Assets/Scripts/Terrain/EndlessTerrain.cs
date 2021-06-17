94202a2caf5e647a447d2a153b5be20e72b0a068
o-fagerstam <55053302+o-fagerstam@users.noreply.github.com>
2021-06-17 20:17:31 +0300

    More random map generation, plus elco pt boat

--- Assets/Scripts/Terrain/EndlessTerrain.cs
+++ Assets/Scripts/Terrain/EndlessTerrain.cs
@@ -12,8 +12,13 @@ namespace Terrain {
         private int chunkVisibleInViewDst;
 
         Dictionary<Vector2, TerrainChunk> _terrainChunkDict = new Dictionary<Vector2, TerrainChunk>();
+        List<TerrainChunk> _terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
+
+        private static MapGenerator _mapGenerator;
+        public Material mapMaterial;
         
         private void Start() {
+            _mapGenerator = FindObjectOfType<MapGenerator>();
             chunkSize = MapGenerator.mapChunkSize - 1;
             chunkVisibleInViewDst = Mathf.RoundToInt(MaxViewDist / chunkSize);
         }
@@ -25,18 +30,27 @@ namespace Terrain {
         }
 
         void UpdateVisibleChunks() {
-            int currentChunkCoordX = Mathf.RoundToInt(viewer.position.x / chunkSize);
-            int currentChunkCoordY = Mathf.RoundToInt(viewer.position.y / chunkSize);
 
+            foreach (TerrainChunk chunk in _terrainChunksVisibleLastUpdate) {
+                chunk.SetVisible(false);
+            }
+            _terrainChunksVisibleLastUpdate.Clear();
+            
+            int currentChunkCoordX = Mathf.RoundToInt(viewerPosition.x / chunkSize);
+            int currentChunkCoordY = Mathf.RoundToInt(viewerPosition.y / chunkSize);
+            
             for (int yOffset = -chunkVisibleInViewDst; yOffset <= chunkVisibleInViewDst; yOffset++) {
-                for (int xOffset = -chunkVisibleInViewDst; xOffset < chunkVisibleInViewDst; xOffset++) {
+                for (int xOffset = -chunkVisibleInViewDst; xOffset <= chunkVisibleInViewDst; xOffset++) {
                     Vector2 viewedChunkCoord = new Vector2(currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
 
                     if (_terrainChunkDict.ContainsKey(viewedChunkCoord)) {
                         _terrainChunkDict[viewedChunkCoord].UpdateTerrainChunk();
+                        if (_terrainChunkDict[viewedChunkCoord].isVisible()) {
+                            _terrainChunksVisibleLastUpdate.Add(_terrainChunkDict[viewedChunkCoord]);
+                        }
                     }
                     else {
-                        _terrainChunkDict.Add(viewedChunkCoord, new TerrainChunk(viewedChunkCoord, chunkSize));
+                        _terrainChunkDict.Add(viewedChunkCoord, new TerrainChunk(viewedChunkCoord, chunkSize, mapMaterial));
                     }
                 }
             }
@@ -46,15 +60,30 @@ namespace Terrain {
             private GameObject meshObject;
             private Vector2 position;
             private Bounds _bounds;
-            public TerrainChunk(Vector2 coord, int size) {
+
+            private MeshRenderer _meshRenderer;
+            private MeshFilter _meshFilter;
+            public TerrainChunk(Vector2 coord, int size, Material material) {
                 position = coord * size;
                 _bounds = new Bounds(position, Vector2.one * size);
                 Vector3 positionV3 = new Vector3(position.x, 0f, position.y);
 
-                meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                meshObject = new GameObject("Terrain Chunk");
+                _meshRenderer = meshObject.AddComponent<MeshRenderer>();
+                _meshFilter = meshObject.AddComponent<MeshFilter>();
+                _meshRenderer.material = material;
                 meshObject.transform.position = positionV3;
-                meshObject.transform.localScale = Vector3.one * size / 10f;
                 SetVisible(false);
+                
+                _mapGenerator.RequestMapData(OnMapDataReceived);
+            }
+
+            void OnMapDataReceived(MapData mapData) {
+                _mapGenerator.RequestMeshData(mapData, OnMeshDataReceived);
+            }
+
+            void OnMeshDataReceived(MeshData meshData) {
+                _meshFilter.mesh = meshData.CreateMesh();
             }
 
             public void UpdateTerrainChunk() {
@@ -65,6 +94,10 @@ namespace Terrain {
 
             public void SetVisible(bool visible) {
                 meshObject.SetActive(visible);
+            }
+
+            public bool isVisible() {
+                return meshObject.activeSelf;
             }
         }
     }
