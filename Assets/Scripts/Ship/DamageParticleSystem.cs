707deae4a99bd38d1f5af2c45940b7e240db358f
o-fagerstam <55053302+o-fagerstam@users.noreply.github.com>
2021-07-18 00:43:00 +0300

    Removed underwater smoke, added particle systems to elco boat

--- Assets/Scripts/Ship/DamageParticleSystem.cs
+++ Assets/Scripts/Ship/DamageParticleSystem.cs
@@ -5,17 +5,35 @@ namespace Ship {
     public class DamageParticleSystem : MonoBehaviour {
         private ParticleSystem[] _particleSystems;
         public DamageParticleSystemType type;
+        public bool isPlaying = false;
 
         private void Awake() {
             _particleSystems = GetComponentsInChildren<ParticleSystem>();
-            foreach (ParticleSystem system in _particleSystems) {
-                system.Stop();
+            Stop();
+        }
+
+        private void Update() {
+            // Stop producing smoke when system goes too far below the surface
+            if (type == DamageParticleSystemType.Smoke && isPlaying && transform.position.y < 0f) {
+                isPlaying = false;
+                foreach (ParticleSystem system in _particleSystems) {
+                    ParticleSystem.EmissionModule emissionModule = system.emission;
+                    emissionModule.enabled = false;
+                }
             }
         }
 
         public void Play() {
+            isPlaying = true;
             foreach (ParticleSystem system in _particleSystems) {
                 system.Play();
+            }
+        }
+
+        public void Stop() {
+            isPlaying = false;
+            foreach (ParticleSystem system in _particleSystems) {
+                system.Stop();
             }
         }
 
---
+++ Assets/Scripts/Ship/ShipParticlesModule.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Ship {
+    public class ShipParticlesModule : MonoBehaviour {
+        private const float FirstSmokePercentage = 0.5f;
+        private ShipMain _shipMain;
+        private ShipDamageModule _damageModule;
+        private DamageParticleSystem[] _smokeParticleSystems;
+        private DamageParticleSystem[] _explosionParticleSystems;
+
+
+        private void Awake() {
+            _shipMain = GetComponentInParent<ShipMain>();
+            _shipMain.OnShipDestroyed += OnShipDestroyed;
+        
+            _damageModule = GetComponentInParent<ShipDamageModule>();
+            _damageModule.OnDamageTaken += OnShipDamaged;
+
+            DamageParticleSystem[] allSystems = GetComponentsInChildren<DamageParticleSystem>();
+            _smokeParticleSystems = allSystems
+                .Where(e => e.type == DamageParticleSystem.DamageParticleSystemType.Smoke)
+                .ToArray();
+            _explosionParticleSystems = allSystems
+                .Where(e => e.type == DamageParticleSystem.DamageParticleSystemType.Explosion)
+                .ToArray();
+        }
+
+        private void OnShipDamaged(object sender, ShipDamageModule.OnDamageTakenArgs args) {
+            float healthPercent = args.healthRemaining / args.maxHealth;
+
+            if (healthPercent < FirstSmokePercentage) {
+                int numOfSystems = _smokeParticleSystems.Length;
+                float smokeFactor = (FirstSmokePercentage - healthPercent) / FirstSmokePercentage;
+                int numSmokes = Mathf.CeilToInt(smokeFactor * numOfSystems);
+                numSmokes = Math.Min(numSmokes, numOfSystems);
+
+                for (int i = 0; i < numSmokes; i++) {
+                    _smokeParticleSystems[i].Play();
+                }
+            }
+
+            if (args.healthRemaining < 0f) {
+                foreach (DamageParticleSystem system in _explosionParticleSystems) {
+                    system.Play();
+                }
+            }
+        }
+
+        private void OnShipDestroyed(ShipMain _) {
+            _shipMain.OnShipDestroyed -= OnShipDestroyed;
+            foreach (DamageParticleSystem system in _explosionParticleSystems) {
+                system.Play();
+            }
+        }
+
+        private void OnDestroy() {
+            _damageModule.OnDamageTaken -= OnShipDamaged;
+            _shipMain.OnShipDestroyed -= OnShipDestroyed;
+        }
+    }
+}
